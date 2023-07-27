dd238abb348b40fc49fee76c6bd12ba879d1c67f
Donghyeok (Henry) Lee <donghyukleee@gmail.com>
2023-07-27 18:31:38 +0100

    Calendar

---
+++ medias/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        "photos/get-url",
+        views.GetUploadURL.as_view(),
+    ),
+    path(
+        "photos/<int:pk>",
+        views.PhotoDetail.as_view(),
+    ),
+]
---
+++ rooms/schema.py
@@ -0,0 +1,21 @@
+# import strawberry
+# import typing
+# from . import types
+# from . import quries
+# from . import mutations
+# from common.permissions import OnlyLoggedIn
+
+
+# @strawberry.type
+# class Query:
+#     all_rooms: typing.List[types.RoomType] = strawberry.field(
+#         resolver=quries.get_all_rooms
+#     )
+#     room: typing.Optional[types.RoomType] = strawberry.field(resolver=quries.get_room)
+
+
+# @strawberry.type
+# class Mutation:
+#     room: typing.Optional[types.RoomType] = strawberry.mutation(
+#         resolver=mutations.add_room, permission_classes=[OnlyLoggedIn]
+#     )
